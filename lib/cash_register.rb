1c1a90afc5196a0bf809784184f354ee2290837c
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-25 10:53:59 -0800

    commit tree 2f043167805142d10b8faf4a9005055d3c07f3b9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader :items
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+        @itemPrices = []
+    end
+
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+        itemObj = { "#{title}": price }
+        @itemPrices << price * quantity
+        quantity.times { @items << title }
+    end
+
+    def apply_discount
+        if discount > 0
+            @total = ((100 - discount) * 0.01 * total).to_i
+            "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @itemPrices.pop
+        if @itemPrices.empty?
+            @total = 0
+        else
+            @total = @itemPrices.sum { |price| price }
+        end
+    end
+        
+end
